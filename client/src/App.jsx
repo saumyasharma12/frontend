f1227eee3535807012ff39023f1e959be7701ea0
saumyasharma12 <139276996+saumyasharma12@users.noreply.github.com>
2025-05-03 15:12:00 +0530

    mail

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,12 @@ import './App.css';
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
-import Login from './pages/Login'
-import Signup from './pages/Signup'
+
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
         <Route
           path="/"
           element={<Home/>}
